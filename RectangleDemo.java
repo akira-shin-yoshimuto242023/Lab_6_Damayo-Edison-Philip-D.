ec1d6bd5d7040b3fc8fca58c7f7cc40b80c3ed63
akira-shin-yoshimuto242023 <edisondamayo@gmail.com>
2025-11-12 20:18:57 +0800

    Update RectangleDemo.java
    Added Rectangle class with area calculation method.

--- RectangleDemo.java
+++ RectangleDemo.java
@@ -1,1 +1,26 @@
+class Rectangle {
+    double length;
+    double width;
+
+    // Constructor
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    // Method to calculate area
+    double calculateArea() {
+        return length * width;
+    }
+}
+
+public class RectangleDemo {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(5.0, 3.0);
+        Rectangle rect2 = new Rectangle(10.0, 2.5);
+
+        System.out.println("Area of rect1: " + rect1.calculateArea());
+        System.out.println("Area of rect2: " + rect2.calculateArea());
+    }
+}
 
